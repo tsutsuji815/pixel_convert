25832a62862ced08c88f2977c769f0dd853dfd3e
tsutsuji815 <tsutsuji815@gmail.com>
2017-05-25 12:44:49 +0900

    Add files via upload
    透過への対応
    ミス修正

--- pixel.py
+++ pixel.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+import sys
 import cv2
 from PIL import Image
 import numpy as np
@@ -14,26 +15,44 @@ n4 = np.array([[0, 1, 0],
               np.uint8)
 
 
-def make_dot(src, k=3, scale=2, color=True, blur=0, erode=0):
+def make_dot(src, k=3, scale=2, color=True, blur=0, erode=0, alpha=True, to_tw=True):
     img_pl = Image.open(src)
-    if img_pl.mode != 'RGB' and img_pl.mode != 'L':
+    if (img_pl.mode == 'RGBA' or img_pl.mode == 'P') and alpha:
+        if img_pl.mode != 'RGBA':
+            img_pl = img_pl.convert('RGBA')
+        alpha_mode = True
+    elif img_pl.mode != 'RGB' and img_pl.mode != 'L':
         img_pl = img_pl.convert('RGB')
+        alpha_mode = False
+    else:
+        alpha_mode = False
     img = np.asarray(img_pl)
-    if color:
+    if color and alpha_mode:
+        a = img[:, :, 3]
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+        h, w, c = img.shape
+    elif color:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         h, w, c = img.shape
     else:
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         h, w = img.shape
         c = 0
+    d_h = int(h / scale)
+    d_w = int(w / scale)
     if erode == 1:
         img = cv2.erode(img, n4, iterations=1)
     elif erode == 2:
         img = cv2.erode(img, n8, iterations=1)
     if blur:
         img = cv2.bilateralFilter(img, 15, blur, 20)
-    img = cv2.resize(img, (int(w / scale), int(h / scale)), interpolation=cv2.INTER_NEAREST)
-    img = cv2.resize(img, (w, h), interpolation=cv2.INTER_NEAREST)
+    img = cv2.resize(img, (d_w, d_h), interpolation=cv2.INTER_NEAREST)
+    if alpha_mode:
+        a = cv2.resize(a, (d_w, d_h), interpolation=cv2.INTER_NEAREST)
+        a = cv2.resize(a, (d_w * scale, d_h * scale), interpolation=cv2.INTER_NEAREST)
+        a[a != 0] = 255
+        if not 0 in a:
+            a[0, 0] = 0
     if color:
         img_cp = img.reshape(-1, c)
     else:
@@ -44,4 +63,17 @@ def make_dot(src, k=3, scale=2, color=True, blur=0, erode=0):
     center = center.astype(np.uint8)
     result = center[label.flatten()]
     result = result.reshape((img.shape))
-    return result
+    result = cv2.resize(result, (d_w * scale, d_h * scale), interpolation=cv2.INTER_NEAREST)
+    if alpha_mode:
+        r, g, b = cv2.split(result)
+        result = cv2.merge((r, g, b, a))
+    elif to_tw:
+        r, g, b = cv2.split(result)
+        a = np.ones(r.shape, dtype=np.uint8) * 255
+        a[0, 0] = 0
+        result = cv2.merge((r, g, b, a))
+    colors = []
+    for res_c in center:
+        color_code = '#{0:02x}{1:02x}{2:02x}'.format(res_c[2], res_c[1], res_c[0])
+        colors.append(color_code)
+    return result, colors
--- pixel_convert.py
+++ pixel_convert.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 config = {'MAX_CONTENT_LENGTH': 1024 * 1024 * 2, 'DEBUG': False}
 app.config.update(config)
 
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('pixel.html')
@@ -21,12 +20,20 @@ def index():
 def post():
     img = request.files['image']
     if not img:
-        error = 'ファイルを選択してね'
+        error='ファイルを選択してね'
         return render_template('pixel.html', error=error)
     k = int(request.form['k'])
     scale = int(request.form['scale'])
     blur = int(request.form['blur'])
     erode = int(request.form['erode'])
+    try:
+        alpha = bool(int(request.form['alpha']))
+    except:
+        alpha = False
+    try:
+        to_tw = bool(int(request.form['to_tw']))
+    except:
+        to_tw = False
     img_name = hashlib.md5(str(dt.datetime.now()).encode('utf-8')).hexdigest()
     img_path = os.path.join('static/img', img_name + os.path.splitext(img.filename)[-1])
     result_path = os.path.join('static/results', img_name + '.png')
@@ -35,11 +42,9 @@ def post():
         if max(img_pl.size) > 1024:
             img_pl.thumbnail((1024, 1024), Image.ANTIALIAS)
             img_pl.save(img_path)
-            # os.remove(img_path)
-            # return render_template('pixel.html', error=error)
-    img_res = make_dot(img_path, k=k, scale=scale, blur=blur, erode=erode)
+    img_res, colors = make_dot(img_path, k=k, scale=scale, blur=blur, erode=erode, alpha=alpha, to_tw=to_tw)
     cv2.imwrite(result_path, img_res)
-    return render_template('pixel.html', org_img=img_path, result=result_path)
+    return render_template('pixel.html', org_img=img_path, result=result_path, colors=colors)
 
 
 @app.errorhandler(413)
